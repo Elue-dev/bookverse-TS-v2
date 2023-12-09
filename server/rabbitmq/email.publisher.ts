9651a9cd29f14c1ba39437c1108ee3bfc5d098f4
Wisdom <eluewisdom@gmail.com>
2023-12-09 11:51:14 +0100

    feat: add a default section to switch statement on queue name

--- server/rabbitmq/email.publisher.ts
+++ server/rabbitmq/email.publisher.ts
@@ -15,63 +15,72 @@ export async function consumeFromRabbitMQAndSendEmail(queueName: string) {
   await channel.assertQueue(queueName, { durable: false });
 
   channel.consume(queueName, async (queueMessage: QueueMessage) => {
-    if (queueMessage) {
-      const [userEmail, username, userId, token] = queueMessage?.content
-        .toString()
-        .split(",");
-
-      const emailOptions: EmailOptions = {
-        SUBJECT: "",
-        BODY: "",
-        SEND_TO: userEmail,
-        SENT_FROM: process.env.EMAIL_USER as string,
-        REPLY_TO: process.env.REPLY_TO as string,
-      };
-
-      switch (queueName) {
-        case "WELCOME_USER_QUEUE":
-          emailOptions.SUBJECT = `Welcome Onboard, ${username}!`;
-          emailOptions.BODY = welcomeEmail(username);
-          break;
-        case "FORGOT_PASSWORD_QUEUE":
-          emailOptions.SUBJECT = "Password reset request";
-          emailOptions.BODY = passwordResetEmail({
-            username: username,
-            url: `${process.env.CLIENT_URL}/auth/reset-password/${token}/${userId}`,
-          });
-          break;
-        case "RESET_PWORD_QUEUE":
-          emailOptions.SUBJECT = `${username}, Your password was successfully reset`;
-          emailOptions.BODY = resetSuccess({
-            username,
-          });
-          break;
-      }
-
-      try {
-        sendEmail(emailOptions);
-        sendSuccessResponse({
-          channel,
-          queueName,
-          consumerTag: queueMessage?.fields.consumerTag,
-        });
-      } catch (error) {
-        console.error("Error sending email", error);
-        sendErrorResponse({
-          channel,
-          queueName,
-          consumerTag: queueMessage?.fields.consumerTag,
-        });
-      }
-
-      channel.ack(queueMessage);
-    } else {
+    if (!queueMessage) {
       sendErrorResponse({
         channel,
         queueName,
         consumerTag: undefined,
       });
+
+      return;
     }
+
+    const [userEmail, username, userId, token] = queueMessage?.content
+      .toString()
+      .split(",");
+
+    const emailOptions: EmailOptions = {
+      SUBJECT: "",
+      BODY: "",
+      SEND_TO: userEmail,
+      SENT_FROM: process.env.EMAIL_USER as string,
+      REPLY_TO: process.env.REPLY_TO as string,
+    };
+
+    switch (queueName) {
+      case "WELCOME_USER_QUEUE":
+        emailOptions.SUBJECT = `Welcome Onboard, ${username}!`;
+        emailOptions.BODY = welcomeEmail(username);
+        break;
+      case "FORGOT_PASSWORD_QUEUE":
+        emailOptions.SUBJECT = "Password reset request";
+        emailOptions.BODY = passwordResetEmail({
+          username: username,
+          url: `${process.env.CLIENT_URL}/auth/reset-password/${token}/${userId}`,
+        });
+        break;
+      case "RESET_PASSWORD_QUEUE":
+        emailOptions.SUBJECT = `${username}, Your password was successfully reset`;
+        emailOptions.BODY = resetSuccess({
+          username,
+        });
+        break;
+      default:
+        sendErrorResponse({
+          channel,
+          queueName,
+          consumerTag: undefined,
+        });
+        return;
+    }
+
+    try {
+      sendEmail(emailOptions);
+      sendSuccessResponse({
+        channel,
+        queueName,
+        consumerTag: queueMessage?.fields.consumerTag,
+      });
+    } catch (error) {
+      console.error("Error sending email", error);
+      sendErrorResponse({
+        channel,
+        queueName,
+        consumerTag: queueMessage?.fields.consumerTag,
+      });
+    }
+
+    channel.ack(queueMessage);
   });
 }
 
